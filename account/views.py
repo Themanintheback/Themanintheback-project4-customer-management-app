4ef29bec6f1d4f554b5a28c50f3490bd3362a2bc
Themanintheback <jonathan.john2501@hotmail.co.uk>
2022-09-10 09:41:31 +0000

    add html templates

--- account/views.py
+++ account/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'account/dashboard.html')
+
+
+def products(request):
+    return render(request, 'account/products.html')
+
+
+def customer(request):
+    return render(request, 'account/customer.html')
