b5959aa69800e39d781c980f157a639cfaf38e38
Themanintheback <jonathan.john2501@hotmail.co.uk>
2022-09-14 03:37:06 +0000

    add rendering of data to templates dashboard and produts

--- account/views.py
+++ account/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 # Create your views here.
+from .models import *
+
 
 def home(request):
-    return render(request, 'account/dashboard.html')
+    orders = Order.objects.all()
+    customer = Customer.objects.all()
+    total_customers = customer.count()
+    total_orders = Order.objects.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders':orders, 'customers':customer,
+    'total_orders':total_orders, 'delivered':delivered,
+    'pending':pending}
+
+    return render(request, 'account/dashboard.html', context)
 
 
 def products(request):
-    return render(request, 'account/products.html')
+    products = Product.objects.all()
+
+    return render(request, 'account/products.html', {'products':products})
 
 
 def customer(request):
