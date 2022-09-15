9b7e10a1c03007259ff88414407aaeb871ec3cca
Themanintheback <jonathan.john2501@hotmail.co.uk>
2022-09-15 03:43:42 +0000

    linked all pages and together and styled the buttons

--- account/views.py
+++ account/views.py
@@ -26,5 +26,11 @@ def products(request):
     return render(request, 'account/products.html', {'products':products})
 
 
-def customer(request):
-    return render(request, 'account/customer.html')
+def customer(request, pk_test):
+    customer = Customer.objects.get(id=pk_test)
+
+    orders = customer.order_set.all()
+    order_count = orders.count()
+
+    context = {'customer':customer , 'orders':orders, 'order_count':order_count}
+    return render(request, 'account/customer.html', context)
